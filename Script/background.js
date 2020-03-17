547401c8b1e00851e20b4f50eaf3659f268d719e
SamhithKakarla <sam.kakarla@gmail.com>
2020-03-16 21:17:17 -0700

    lol

--- Script/background.js
+++ Script/background.js
@@ -4,4 +4,4 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({numSubmissions: '0' }, function () {
         //alert('numSubmissions is : ' + );
     });
-});
+})
--- Script/btn.js
+++ Script/btn.js
@@ -1,6 +1,10 @@
 // Pure JS:
 document.addEventListener('DOMContentLoaded', function () {
+
+   if(chrome.getcookie("blah")){ 
     document.getElementById("btn_submit").addEventListener("click", btnSubmitHandler);
+    chrome.setcookie("blah") == " ";
+   }
 });
 
 
@@ -8,21 +12,14 @@ function btnSubmitHandler() {
 
     chrome.storage.sync.get(['numSubmissions'], function (result) {
         var counter = result.numSubmissions;
-        var counterInt = parseInt(counter);
-        
         // enter logic here
-        if(counterInt < 7){
             var form = document.forms["myForm"];
             var data = "";
             var i;
             for (i = 0; i < form.length; i++) {
                 data += form.elements[i].value + "  ";
             }  
-            alert(data + "  " + counterInt); 
-            chrome.storage.sync.set({numSubmissions: counterInt + 1});
-        } else {
-            alert('No more submissions allowed.');
-        }
+            alert(data); 
 
     });
 }
