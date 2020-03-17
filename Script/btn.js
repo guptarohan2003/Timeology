61eb57e2b2713c120674be2810ecc4634d4339a2
guptarohan2003 <guptarohan2003@gmail.com>
2020-03-16 19:17:27 -0700

    counter lol

--- Script/btn.js
+++ Script/btn.js
@@ -4,40 +4,25 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-// The handler also must go in a .js file
 function btnSubmitHandler() {
 
-    var c = -1;
     chrome.storage.sync.get(['numSubmissions'], function (result) {
-        //alert('result is : ' + result.numSubmissions);
         var counter = result.numSubmissions;
-        alert(counter);
         var counterInt = parseInt(counter);
-
+        
         // enter logic here
-
-        chrome.storage.sync.set({numSubmissions: counterInt + 1}, function () {
-            // chrome.storage.sync.get(['numSubmissions'], function (result) {
-            //     alert('result is : ' + result.numSubmissions);
-            // });
-        });
+        if(counterInt < 7){
+            var form = document.forms["myForm"];
+            var data = "";
+            var i;
+            for (i = 0; i < form.length; i++) {
+                data += form.elements[i].value + "  ";
+            }  
+            alert(data + "  " + counterInt); 
+            chrome.storage.sync.set({numSubmissions: counterInt + 1});
+        } else {
+            alert('No more submissions allowed.');
+        }
 
     });
-
-        // console.log('Value currently is ' + result.key);
-
-        // if (numSubmissions < 8) {
-        //     var x = document.forms["myForm"];
-        //     var data = "";
-        //     var i;
-        //     for (i = 0; i < x.length; i++) {
-        //         data += x.elements[i].value + "  ";
-
-        //     }   
-        //     numSubmissions++;        
-        // }
-       
-
-
-    // numSubmissions = 5;
 }
