b7d265b7e67dcca817c795aa7254d8394f78d050
guptarohan2003 <guptarohan2003@gmail.com>
2020-03-16 18:59:00 -0700

    fix

---
+++ Script/background.js
@@ -0,0 +1,7 @@
+
+'use strict';
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({numSubmissions: '0' }, function () {
+        //alert('numSubmissions is : ' + );
+    });
+});
--- Script/btn.js
+++ Script/btn.js
@@ -3,16 +3,41 @@ document.addEventListener('DOMContentLoaded', function () {
     document.getElementById("btn_submit").addEventListener("click", btnSubmitHandler);
 });
 
+
 // The handler also must go in a .js file
 function btnSubmitHandler() {
-    // alert('btnSubmitHandler called');
 
-    var x = document.forms["myForm"];
-    var data = "";
-    var i;
-    for (i = 0; i < x.length; i++) {
-        data += x.elements[i].value + "  ";
+    var c = -1;
+    chrome.storage.sync.get(['numSubmissions'], function (result) {
+        //alert('result is : ' + result.numSubmissions);
+        var counter = result.numSubmissions;
+        alert(counter);
+        var counterInt = parseInt(counter);
 
-    }
-    alert(data);
+        // enter logic here
+
+        chrome.storage.sync.set({numSubmissions: counterInt + 1}, function () {
+            // chrome.storage.sync.get(['numSubmissions'], function (result) {
+            //     alert('result is : ' + result.numSubmissions);
+            // });
+        });
+
+    });
+
+        // console.log('Value currently is ' + result.key);
+
+        // if (numSubmissions < 8) {
+        //     var x = document.forms["myForm"];
+        //     var data = "";
+        //     var i;
+        //     for (i = 0; i < x.length; i++) {
+        //         data += x.elements[i].value + "  ";
+
+        //     }   
+        //     numSubmissions++;        
+        // }
+       
+
+
+    // numSubmissions = 5;
 }
