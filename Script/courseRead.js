42b058e5c34614141e637a59ec4ae9d2ceaa4ad9
guptarohan2003 <guptarohan2003@gmail.com>
2020-03-21 18:38:21 -0700

    courses

--- Script/courseRead.js
+++ Script/courseRead.js
@@ -1,12 +1,29 @@
-alert('inside courseRead');
-
 $(document).ready(function () {
-    //debugger;
-    alert('inside readydoc for courses');
-    var list = $("body").find(".mycourses");
-    $("li").each(function(index){
-        //console.log(list.get(index));
-        console.log(index);
+    //courses array
+    var courses = [];
+
+    //for each courses title push into array
+    $(".course-title").each(function(index, content){
+        var str = content.innerHTML;
+        courses.push(str);
     });
-    //console.log(list.clone().html());
+
+    //for each print courses content
+    courses.forEach(function(item, index){
+        console.log(item);
+    });
+    //console.log();
+    var str = courses[0].toString();
+    chrome.storage.sync.set({ class1:  str});
+    chrome.storage.sync.set({ class2: 'lol' });
+    chrome.storage.sync.set({ class3: 'lol' });
+    chrome.storage.sync.set({ class4: 'lol' });
+    chrome.storage.sync.set({ class5: 'lol' });
+    chrome.storage.sync.set({ class6: 'lol' });
+    chrome.storage.sync.set({ class7: 'lol' });
+
+    chrome.storage.sync.get(['class1'], function(value){
+        console.log(value);
+    })
+    //console.log();
 });
