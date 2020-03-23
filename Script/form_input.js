f61ca7b8a9cd932cefe0b5aa9418b13ad100d62e
SamhithKakarla <sam.kakarla@gmail.com>
2020-03-22 18:41:39 -0700

    storage

--- Script/form_input.js
+++ Script/form_input.js
@@ -23,22 +23,39 @@ function btnSubmitHandler() {
     // chrome.storage.sync.set({ class6: form.elements[15].value.toString() });
     //chrome.storage.sync.set({ class7: form.elements[0].value.toString() });
 
-    var atime1 = parseInt(form.elements[1].value) * 60 + parseInt(form.elements[2].value);
-    chrome.storage.sync.set({ time1: atime1.toString() });
+    // storing all the assignment times
+    var atime1 = parseInt(form.elements[0].value) * 60 + parseInt(form.elements[1].value);
+    chrome.storage.sync.set({ atime1: atime1.toString() });
     var atime2 = parseInt(form.elements[4].value) * 60 + parseInt(form.elements[5].value);
-    chrome.storage.sync.set({ time2: atime2.toString() });
-    var atime3 = parseInt(form.elements[7].value) * 60 + parseInt(form.elements[8].value);
-    chrome.storage.sync.set({ time3: atime3.toString() });
-    var atime4 = parseInt(form.elements[10].value) * 60 + parseInt(form.elements[11].value);
-    chrome.storage.sync.set({ time4: atime4.toString() });
-    var atime5 = parseInt(form.elements[13].value) * 60 + parseInt(form.elements[14].value);
-    chrome.storage.sync.set({ time5: atime5.toString() });
-    var atime6 = parseInt(form.elements[16].value) * 60 + parseInt(form.elements[17].value);
-    chrome.storage.sync.set({ time6: atime6.toString() });
-    //var atime7 = parseInt(form.elements[20].value)*60 + parseInt(form.elements[21].value);
-    //chrome.storage.sync.set({ time7: atime7.toString() });
+    chrome.storage.sync.set({ atime2: atime2.toString() });
+    var atime3 = parseInt(form.elements[8].value) * 60 + parseInt(form.elements[9].value);
+    chrome.storage.sync.set({ atime3: atime3.toString() });
+    var atime4 = parseInt(form.elements[12].value) * 60 + parseInt(form.elements[13].value);
+    chrome.storage.sync.set({ atime4: atime4.toString() });
+    var atime5 = parseInt(form.elements[16].value) * 60 + parseInt(form.elements[17].value);
+    chrome.storage.sync.set({ atime5: atime5.toString() });
+    var atime6 = parseInt(form.elements[20].value) * 60 + parseInt(form.elements[21].value);
+    chrome.storage.sync.set({ atime6: atime6.toString() });
+    var atime7 = parseInt(form.elements[24].value)*60 + parseInt(form.elements[25].value);
+    chrome.storage.sync.set({ atime7: atime7.toString() });
 
-    alert(data);
+    //storing all the project times
+    var ptime1 = parseInt(form.elements[2].value) * 60 + parseInt(form.elements[3].value);
+    chrome.storage.sync.set({ ptime1: ptime1.toString() });
+    var ptime2 = parseInt(form.elements[6].value) * 60 + parseInt(form.elements[7].value);
+    chrome.storage.sync.set({ ptime2: ptime2.toString() });
+    var ptime3 = parseInt(form.elements[10].value) * 60 + parseInt(form.elements[11].value);
+    chrome.storage.sync.set({ ptime3: ptime3.toString() });
+    var ptime4 = parseInt(form.elements[14].value) * 60 + parseInt(form.elements[15].value);
+    chrome.storage.sync.set({ ptime4: ptime4.toString() });
+    var ptime5 = parseInt(form.elements[18].value) * 60 + parseInt(form.elements[19].value);
+    chrome.storage.sync.set({ ptime5: ptime5.toString() });
+    var ptime6 = parseInt(form.elements[22].value) * 60 + parseInt(form.elements[23].value);
+    chrome.storage.sync.set({ ptime6: ptime6.toString() });
+    var ptime7 = parseInt(form.elements[26].value)*60 + parseInt(form.elements[27].value);
+    chrome.storage.sync.set({ ptime7: ptime7.toString() });
+
+    //alert(data);
 
     //reset form
     form.reset();
