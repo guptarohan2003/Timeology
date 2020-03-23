f086aa16452bdda8160aa1566ad2bc428f4d5b8a
guptarohan2003 <guptarohan2003@gmail.com>
2020-03-22 21:50:14 -0700

    a

--- Script/background.js
+++ Script/background.js
@@ -24,10 +24,11 @@ chrome.runtime.onInstalled.addListener(function (details) {
     //     alert(value);
     // });
     
+    chrome.storage.sync.set({enabled: "false"});
     chrome.storage.sync.set({ coursesRead: 'false' });
     chrome.storage.sync.set({ totalT: "0"});
 
-    chrome.tabs.create({ url: 'https://fuhsd.schoology.com/courses' });
+    // chrome.tabs.create({ url: 'https://fuhsd.schoology.com/courses' });
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
@@ -40,4 +41,45 @@ chrome.runtime.onInstalled.addListener(function (details) {
     });
 });
 
+// document.addEventListener('DOMContentLoaded', function () {
+//     //try {
+//     document.getElementById("enableTimeology").innerHTML == 'Enable Timeology' ? disableTimeology() : enableTimeology();
+//     //}
+//     document.getElementById("enableTimeology").addEventListener("click", function () {
+//         if(document.getElementById("enableTimeology").innerHTML == 'Enable Timeology'){
+//             enableTimeology();
+//             document.getElementById("enableTimeology").innerHTML = "Disable Timeology";
+//         } else {
+//             disableTimeology();
+//             document.getElementById("enableTimeology").innerHTML = "Enable Timeology";
+//         }
+//         chrome.tabs.reload();
+//     });
+
+//     document.getElementById("go_to_form").addEventListener("click", function () {
+//         //create form
+//         chrome.storage.sync.get(['coursesRead'], function(val){
+//             var read = val.coursesRead;
+//             if(read == "false"){
+//                 document.getElementById("go_to_form").innerHTML = "Click me to go to Your courses page so we can get your courses!";
+//                 chrome.tabs.create({ url: "https://fuhsd.schoology.com/courses" });
+//             }else{
+//                 document.getElementById("go_to_form").innerHTML = "Click me to go to the form";
+//                 chrome.tabs.create({ url: "form.html" });
+//             }
+//         });
+//     });
+// });
+
+// function enableTimeology(){
+//     // document.getElementById("time").style.display = "inline";
+//     document.getElementById("timedisplay").style.display = "inline";
+//     document.getElementById("go_to_form").style.display = "inline";
+// }
+
+// function disableTimeology(){
+//     document.getElementById("go_to_form").style.display = "none";
+//     document.getElementById("timedisplay").style.display = "none";
+//     // document.getElementById("time").style.display = "none";
+// }
 
--- Script/popup.js
+++ Script/popup.js
@@ -1,40 +1,67 @@
 document.addEventListener('DOMContentLoaded', function () {
     //try {
-    document.getElementById("enableTimeology").innerHTML == 'Enable Timeology' ? disableTimeology() : enableTimeology();
+    //document.getElementById("enableTimeology").innerHTML == 'Enable Timeology' ? disableTimeology() : enableTimeology();
     //}
+    chrome.storage.sync.get(['coursesRead'], function (val) {
+        var read = val.coursesRead;
+        if (read == "true") {
+            document.getElementById("go_to_form").innerHTML = "Click me to go to the form";
+        }
+    });
+    chrome.storage.sync.get(['enabled'], function(val){
+        var value = val.enabled;
+        if(value == 'true'){
+            if (document.getElementById("enableTimeology").innerHTML = 'Disable Timeology') 
+            enableTimeology();
+        } else {
+            document.getElementById("enableTimeology").innerHTML = "Enable Timeology";
+            disableTimeology();
+        }
+    });
+    
     document.getElementById("enableTimeology").addEventListener("click", function () {
-        if(document.getElementById("enableTimeology").innerHTML == 'Enable Timeology'){
+        if (document.getElementById("enableTimeology").innerHTML == 'Enable Timeology') {
             enableTimeology();
             document.getElementById("enableTimeology").innerHTML = "Disable Timeology";
+            chrome.storage.sync.set({enabled: "true"});
         } else {
             disableTimeology();
             document.getElementById("enableTimeology").innerHTML = "Enable Timeology";
+            chrome.storage.sync.set({enabled: "false"});
+
         }
     });
 
     document.getElementById("go_to_form").addEventListener("click", function () {
         //create form
-        chrome.storage.sync.get(['coursesRead'], function(val){
+        chrome.storage.sync.get(['coursesRead'], function (val) {
             var read = val.coursesRead;
-            if(read == "false"){
-                document.getElementById("go_to_form").innerHTML = "Click me to go to Your courses page so we can get your courses!";
-                chrome.tabs.create({ url: "https://fuhsd.schoology.com/courses" });
-            }else{
-                document.getElementById("go_to_form").innerHTML = "Click me to go to the form";
+            if (read == "true") {
                 chrome.tabs.create({ url: "form.html" });
+            } else {
+                chrome.tabs.create({ url: "https://fuhsd.schoology.com/courses" });
             }
         });
     });
 });
 
-function enableTimeology(){
+function enableTimeology() {
     // document.getElementById("time").style.display = "inline";
     document.getElementById("timedisplay").style.display = "inline";
     document.getElementById("go_to_form").style.display = "inline";
+    // chrome.tabs.reload();
+    // chrome.storage.sync.get(['coursesRead'], function (val) {
+    //     var value = val.coursesRead;
+    //     if (val == "false") {
+    //         //chrome.tabs.create({ url: 'https://fuhsd.schoology.com/courses' });
+    //     }
+    // });
+
 }
 
-function disableTimeology(){
+function disableTimeology() {
     document.getElementById("go_to_form").style.display = "none";
     document.getElementById("timedisplay").style.display = "none";
     // document.getElementById("time").style.display = "none";
+    // chrome.tabs.reload();
 }
