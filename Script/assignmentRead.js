773d31e37ff0692a7ee81a28426d6d208fc0e4ad
guptarohan2003 <guptarohan2003@gmail.com>
2020-03-22 17:52:24 -0700

    idk

--- Script/assignmentRead.js
+++ Script/assignmentRead.js
@@ -1,60 +1,24 @@
 $(document).ready(function () {
     var assignments = []
-
-    // alert('inside assignmentread.js');
-
-
-    //
-    // var main_content_wrapper = $('.sHome-processed')[0];
-    // var main_content_wrapper_html = main_content_wrapper.outerHTML;
-    // console.log(main_content_wrapper_html);
-
-    // create a element from html
-
-    // var dom_nodes = $($.parseHTML(main_content_wrapper_html));
-    // console.log(dom_nodes);
-    //
-    
-    // var e = main_content_wrapper.find('.realm-title-course');
-    //console.log(e.outerHTML);
-
-    //var main = $($('.upcoming-list')[0].offsetParent).find('.realm-title-course');
-    //var element = $('.infotip');
-    // 
-    // if (element !== null && element !== undefined) {
-    // e.each(function (index, content) {
-    //     alert('found assig');
-
-    //     //var str = content.attr("aria-label");
-    //     alert(content.innerHTML);
-    // });
-    // }
-
-    $.ajax({     
+    $.ajax({
         type: "GET",
         url: '/home/upcoming_ajax',
         data: '',
         success: function (data) {
-            
-            console.log(data);
-            // debugger
-            // var string = '<div><input type="text" value="val" /></div>';
+
+            // create a empty div, with content from ajax output
             var object = $('<div/>').html(data.html).contents();
-            console.log(object[1].outerHTML);
-            //var main = $(object[1]).find('.sCommonInfotip-processed');
 
-            // alert( object.find('input').val() );
-            
-            // create a element from html
-
-            // var dom_nodes = $.parseHTML(data);
-            // var dom_nodes = $($.parseHTML('<div><input type="text" value="val" /></div>'));
-            // var dom_nodes = $($.parseHTML(data));
-
-            // console.log(dom_nodes);
-            
-            // var e = dom_nodes.find('.realm-title-course');
-            // console.log(e.outerHTML);
+            var h4_list = $(object[1]).find('h4');
+            $.each(h4_list, function (index, element) {
+                
+                // create a dummuy object
+                var dummy = $('<div/>').html(element.outerHTML).contents();
+                var course = dummy.find('.realm-title-course');
+                if (course && course.length > 0 && course[0].outerText) {
+                    console.log(course[0].outerText);
+                }
+            });
 
         },
         dataType: "json"
