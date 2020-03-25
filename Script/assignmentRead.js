4f7b768fe51680dae84d9f2b3f794fb6af498d7c
guptarohan2003 <guptarohan2003@gmail.com>
2020-03-25 00:36:40 -0700

    fixes

--- Script/assignmentRead.js
+++ Script/assignmentRead.js
@@ -70,84 +70,41 @@ $(document).ready(function () {
                 dataType: "json"
             });
 
-            //create html text here that says time of hw
-            chrome.storage.sync.get(['enabled'], function (val) {
-                if (val.enabled == 'true') {
-                    chrome.storage.sync.get(['numAssigments1'], function (val) {
-                        var assig1 = parseInt(val.numAssigments1)
-                        chrome.storage.sync.get(['atime1'], function (val) {
-                            var time1 = parseInt(val.atime1);
-                            var at1 = time1 * assig1;
+            var assign_array = [
+                'numAssigments1',
+                'numAssigments2',
+                'numAssigments3',
+                'numAssigments4',
+                'numAssigments5',
+                'numAssigments6',
+                'numAssigments7',
+                'atime1',
+                'atime2',
+                'atime3',
+                'atime4',
+                'atime5',
+                'atime6',
+                'atime7'
+            ];
 
-                            chrome.storage.sync.get(['numAssigments2'], function (val) {
-                                var assig2 = parseInt(val.numAssigments2)
-                                chrome.storage.sync.get(['atime2'], function (val) {
-                                    var time2 = parseInt(val.atime2);
-                                    var at2 = assig2 * time2;
+            chrome.storage.sync.get(assign_array, function (items) {
+                var at1 = parseInt(items.atime1) * parseInt(items.numAssigments1);
+                var at2 = parseInt(items.atime2) * parseInt(items.numAssigments2);
+                var at3 = parseInt(items.atime3) * parseInt(items.numAssigments3);
+                var at4 = parseInt(items.atime4) * parseInt(items.numAssigments4);
+                var at5 = parseInt(items.atime5) * parseInt(items.numAssigments5);
+                var at6 = parseInt(items.atime6) * parseInt(items.numAssigments6);
+                var at7 = parseInt(items.atime7) * parseInt(items.numAssigments7);
 
-                                    chrome.storage.sync.get(['numAssigments3'], function (val) {
-                                        var assig3 = parseInt(val.numAssigments3)
-                                        chrome.storage.sync.get(['atime3'], function (val) {
-                                            var time3 = parseInt(val.atime3);
-                                            var at3 = time3 * assig3;
-
-                                            chrome.storage.sync.get(['numAssigments4'], function (val) {
-                                                var assig4 = parseInt(val.numAssigments4)
-                                                chrome.storage.sync.get(['atime4'], function (val) {
-                                                    var time4 = parseInt(val.atime4);
-                                                    var at4 = time4 * assig4;
-
-                                                    chrome.storage.sync.get(['numAssigments5'], function (val) {
-                                                        var assig5 = parseInt(val.numAssigments5);
-                                                        chrome.storage.sync.get(['atime5'], function (val) {
-                                                            var time5 = parseInt(val.atime5);
-                                                            var at5 = time5 * assig5;
-
-                                                            chrome.storage.sync.get(['numAssigments6'], function (val) {
-                                                                var assig6 = parseInt(val.numAssigments6);
-                                                                chrome.storage.sync.get(['atime6'], function (val) {
-                                                                    var time6 = parseInt(val.atime6);
-                                                                    var at6 = time6 * assig6;
-
-                                                                    chrome.storage.sync.get(['numAssigments7'], function (val) {
-                                                                        var assig7 = parseInt(val.numAssigments7);
-                                                                        chrome.storage.sync.get(['atime7'], function (val) {
-                                                                            var time7 = parseInt(val.atime7);
-                                                                            var at7 = time7 * assig7;
-
-                                                                            chrome.storage.sync.get(['totalT'], function (val) {
-                                                                                var totalZ = val.totalT;
-                                                                                totalZ = at1 + at2 + at3 + at4 + at5 + at6 + at7;
-
-                                                                                var hrs = Math.floor(totalZ / 60);
-                                                                                var min = totalZ % 60;
-                                                                                console.log(hrs + "  " + min);
-                                                                                var str = "You have about <b>" + hrs + " hrs and " + min + " min</b> of HW today! Good Luck!!   <br>  - TIMEOLOGY";
-                                                                                $("#right-column").prepend('<div id="timeology time" style="padding-left: 10px; padding-right: 10px; border: 1px solid #4CAF50; border-radius: 15px"><table> <tr> <th>Amount of HW Today</th> </tr> <tr> <td id = "time display">' + str + '</td> </tr></table></div>');
-                                                                                // document.getElementById('timedisplay').innerHTML = 'You have about ' + hrs + " hrs and " + min + ' min of HW today!';
-                                                                            });
-                                                                        });
-
-                                                                    });
-                                                                });
-
-                                                            });
-                                                        });
-
-                                                    });
-                                                });
-
-                                            });
-                                        });
-
-                                    });
-                                });
-                            });
-
-                        });
-                    });
-                }
+                var totalZ = at1 + at2 + at3 + at4 + at5 + at6 + at7;                
+                var hrs = Math.floor(totalZ / 60);
+                var min = totalZ % 60;
+                console.log(hrs + "  " + min);
+                var str = "You have about <b>" + hrs + " hrs and " + min + " min</b> of HW today! Good Luck!!   <br>  - TIMEOLOGY";
+                $("#right-column").prepend('<div id="timeology time" style="padding-left: 10px; padding-right: 10px; border: 1px solid #4CAF50; border-radius: 15px"><table> <tr> <th>Amount of HW Today</th> </tr> <tr> <td id = "time display">' + str + '</td> </tr></table></div>');
+           
             });
+
 
             // var obj = $('.a...');
             // $('.upcoming-list').append('<button>asddgsgdsd</button>');
--- Script/popup.js
+++ Script/popup.js
@@ -43,6 +43,13 @@ document.addEventListener('DOMContentLoaded', function () {
     if(ele){
         ele.addEventListener("click", function () {
             chrome.tabs.create({ url: "form.html" });
+
+            // document.body.innerHTML += '<dialog>This is a dialog.<br><button>Close</button></dialog>';
+            // var dialog = document.querySelector("dialog")
+            // dialog.querySelector("button").addEventListener("click", function() {
+            //     dialog.close()
+            // })
+            // dialog.showModal()
         });
     }
     
